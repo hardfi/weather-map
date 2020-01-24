602c2e34d68a30626287f5379c0f07934294f552
Kamil Polok <k.polok@inwebit.pl>
2020-01-24 14:06:24 +0100

    Updates

---
+++ precache-manifest.36663cb84399bb7470eea9cf46956fb0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d05e5919a9a9adbc7bda37dd62e76cdd",
+    "url": "/weather-map/index.html"
+  },
+  {
+    "revision": "07d0958166282f010f15",
+    "url": "/weather-map/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "facfb87244c2f6824472",
+    "url": "/weather-map/static/css/main.cd3f3a51.chunk.css"
+  },
+  {
+    "revision": "07d0958166282f010f15",
+    "url": "/weather-map/static/js/2.8869c0a4.chunk.js"
+  },
+  {
+    "revision": "7e932b5a27afea83c413a61e6c985f6e",
+    "url": "/weather-map/static/js/2.8869c0a4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "facfb87244c2f6824472",
+    "url": "/weather-map/static/js/main.d9b1db56.chunk.js"
+  },
+  {
+    "revision": "cd5b6245206c38bb8511",
+    "url": "/weather-map/static/js/runtime-main.4f0d9723.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/weather-map/static/media/logo.5d5d9eef.svg"
+  }
+]);
